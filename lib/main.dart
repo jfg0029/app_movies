6150bbf199ef5f79a98935e950e30500069b767b
jfg0029 <jfg0029@alu.medac.es>
2025-05-10 19:44:39 +0200

    Integración de la app de Flutter con Firebase y Preparación del entrorno de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_with_firebase_ow/firebase_options.dart';
 import 'package:flutter_with_firebase_ow/routes/app_pages.dart';
 import 'package:flutter_with_firebase_ow/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 
-void main() => runApp(const MyApp());
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(const MyApp());}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
